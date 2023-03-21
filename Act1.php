1b9b7eea6992ef3719017960217629ebb9840766
Melanio III Flores <mflores.intern.integratr@outlook.com>
2023-03-21 11:33:39 +0800

    initial commit

---
+++ Act1.php
@@ -0,0 +1,38 @@
+<?php
+$course1 = [
+    'BSIT',
+    'BSBA',
+    'HMO',
+    'NC2',
+    'ENGINEERING',
+    'NURSE',
+    'ARCHITECH',
+     'TOURISM'
+];
+
+$course2 = [
+    'HMO',
+    'ENGINEERING',
+    'NURSE'
+];
+
+$result = array_diff($course1,$course2);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1><?php
+     print_r( $result)?>
+     </h1>
+    
+</body>
+</html>
